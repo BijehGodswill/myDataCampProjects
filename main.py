5af9301ff3075e1bd44865ceb9bde48091ebc4df
Bijeh Godswill <bijehgodswill@gmail.com>
2023-03-15 03:55:30 +0100

    Using plot and scatter graphs

---
+++ main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+year = [1950, 1970, 1990, 2010]
+pop = [2.519, 3.692, 5.263, 6.972]
+plt.scatter(year, pop)
+plt.show()
+
+# Print the last item from year and pop
+print(year, pop)
+
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+
+# Display the plot with plt.show()
+plt.show()
